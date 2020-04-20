86f4b9f17e0bff9ec3cec43af3da1f668e9905d3
lusongsong2 <lusongsong@jd.com>
2020-04-20 11:14:19 +0800

    日志修改

--- src/main/java/com/fox/api/annotation/aspect/log/LogShowTimeAnt.java
+++ src/main/java/com/fox/api/annotation/aspect/log/LogShowTimeAnt.java
@@ -1,11 +1,12 @@
 package com.fox.api.annotation.aspect.log;
 
-
 import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
 @Documented
+/**
+ * @author lusongsong
+ */
 public @interface LogShowTimeAnt {
-    String value() default "";
 }
--- src/main/java/com/fox/api/annotation/aspect/log/LogShowTimeAspect.java
+++ src/main/java/com/fox/api/annotation/aspect/log/LogShowTimeAspect.java
@@ -6,12 +6,17 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
 
 @Aspect
 @Component
+/**
+ * @author lusongsong
+ */
 public class LogShowTimeAspect {
 
     @Pointcut("@annotation(com.fox.api.annotation.aspect.log.LogShowTimeAnt)")
@@ -25,7 +30,8 @@ public class LogShowTimeAspect {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String className = joinPoint.getTarget().getClass().getSimpleName();
         String methodName = joinPoint.getSignature().getName();
-        System.out.println(className + ":" + methodName + ":" + breakpoint + ":" + df.format(System.currentTimeMillis()));
+        Logger logger = LoggerFactory.getLogger(className);
+        logger.info(className + ":" + methodName + ":" + breakpoint + ":" + df.format(System.currentTimeMillis()));
     }
 
     @Before("logShowTimePointcut()")
