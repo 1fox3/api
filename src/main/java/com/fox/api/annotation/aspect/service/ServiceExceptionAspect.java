9884d9af84e199ed42117741eadfe9dc11ab90ea
lusongsong2 <lusongsong@jd.com>
2020-05-11 15:08:38 +0800

    service运行发生异常是，转为ServiceExpection

---
+++ src/main/java/com/fox/api/annotation/aspect/service/ServiceExceptionAspect.java
@@ -0,0 +1,30 @@
+package com.fox.api.annotation.aspect.service;
+
+import com.fox.api.exception.self.ServiceException;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+/**
+ * 服务层运行时将异常转为ServiceException
+ * @author lusongsong
+ */
+public class ServiceExceptionAspect {
+    @Pointcut("@within(org.springframework.stereotype.Service)")
+    public void serviceExceptionAnt() {}
+
+    @Around("serviceExceptionAnt()")
+    public void around(ProceedingJoinPoint proceedingJoinPoint){
+        try {
+            proceedingJoinPoint.proceed();//有这一句，目标方法才会执行，不然目标方法无法执行
+        } catch (ServiceException se) {
+            throw se;
+        } catch (Throwable throwable) {
+            throw new ServiceException(1, throwable.getMessage());
+        }
+    }
+}
